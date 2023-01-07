47d91c19d666757f95c756f400609d3f6f9844de
Kapeet <itamar246@gmail.com>
2023-01-07 23:58:07 +0200

    generate tailwind css, add source code nav item, add invite to server button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
+  const inviteUrlOrNull = process.env.DISCORD_INVITE_LINK;
+
   return (
     <div className="relative w-full p-5 text-white shadow-lg">
-      <div className="rounded-lg bg-gray-900 p-5">
+      <div className="flex flex-col gap-10 rounded-lg bg-gray-900 p-5">
         <div className="flex items-start justify-between">
           <div>
             <h1 className="font-bold">Canni</h1>
@@ -20,6 +23,16 @@ export default function Home() {
             alt="Canni bot's discord profile picture"
           />
         </div>
+        {inviteUrlOrNull && (
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            href={`${inviteUrlOrNull}`}
+            className="rounded-lg bg-indigo-500 p-3 text-center font-bold"
+          >
+            Invite Canni
+          </Link>
+        )}
       </div>
     </div>
   );
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,12 +1,11 @@
-'use client';
 import React from 'react';
 import NavbarIcon from './NavbarIcon';
-import { FaInfoCircle, FaHome, FaBook } from 'react-icons/fa';
+import { FaInfoCircle, FaHome, FaBook, FaGithubAlt } from 'react-icons/fa';
 import Link from 'next/link';
-
 export default function Navbar() {
+  const githubUrlOrFallback = process.env.GITHUB_URL || '/';
   return (
-    <nav className="m-0 flex h-screen w-16 flex-col bg-gray-900 text-white">
+    <nav className="m-0 flex h-screen w-20 flex-col bg-gray-900 text-white">
       <Link href="/">
         <NavbarIcon icon={<FaHome />} text="Home" />
       </Link>
@@ -15,6 +14,13 @@ export default function Navbar() {
       </Link>
       <Link href="/about">
         <NavbarIcon icon={<FaInfoCircle />} text="About" />
+      </Link>
+      <Link
+        target="_blank"
+        rel="noopener noreferrer"
+        href={githubUrlOrFallback}
+      >
+        <NavbarIcon icon={<FaGithubAlt />} text="Source code" />
       </Link>
     </nav>
   );
