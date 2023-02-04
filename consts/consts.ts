25673c6ccd3c01abdeddff6549ac946eca71fbde
Kapeet <itamar246@gmail.com>
2023-02-04 16:04:17 +0200

    revert

--- consts/consts.ts
+++ consts/consts.ts
@@ -1,6 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : `https://${
-        process.env.NEXT_PUBLIC_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL
-      }`;
+    : `https://${process.env.NEXT_PUBLIC_URL}`;
